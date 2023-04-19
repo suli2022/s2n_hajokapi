56e97e03b84983732d91df5b51eb1aa315d8ae1b
Sallai András <janos@zold.lan>
2023-04-19 11:31:44 +0200

    Kész

---
+++ app/Http/Controllers/ShipController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ship;
+use Illuminate\Http\Request;
+
+class ShipController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Ship::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Ship::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ship  $ship
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ship $ship)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ship  $ship
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $ship = Ship::find($id);
+        $ship->update($request->all());
+        return $ship;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ship  $ship
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Ship::destroy($id);
+    }
+}
